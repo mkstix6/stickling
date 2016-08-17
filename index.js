8549213199da16591e07d58dc9362526d94b7585
mkstix6 <mkstix6@gmail.com>
2016-08-17 08:17:36 +0100

    attempt at basic node serve script

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 "use strict";
+var http = require('http');
+var fs = require('fs');
 
-let mposit = [0, 0]
-
-document.onmousemove = handleMouseMove;
-
-;
-(function attract() {
-    let card = document.getElementById('mecard')
-        // cposition = card.getBoundingClientRect()
-
-    function renderattract() {
-        // cposition = card.getBoundingClientRect()
-        card.style.transform = "rotateX(" + (mposit[1] * 0.01) + "deg) rotateY(" + (mposit[0] * -0.01) + "deg)"
-
-        // console.log(cposition)
-        // console.log("position", mposit)
-        window.requestAnimationFrame(renderattract)
-    }
-    window.requestAnimationFrame(renderattract)
-})()
-
-function handleMouseMove(event) {
-
-    mposit = [event.pageX - 500, event.pageY - 500]
-}
+http.createServer(function(req, res) {
+    fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+}).listen(8080);
