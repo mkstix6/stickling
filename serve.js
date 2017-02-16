b0d52951be46315b00e1e8736f6ab03aeccb1212
mkstix6 <mkstix6@gmail.com>
2017-02-16 23:12:45 +0000

    root folder adjustment

--- serve.js
+++ serve.js
@@ -2,6 +2,6 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/'))
 
 app.listen(process.env.PORT || 3000)
