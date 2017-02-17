5724de14f1b06c07e00891f028c8917f246cff4a
mkstix6 <mkstix6@gmail.com>
2017-02-17 21:12:08 +0000

    attampt again

--- serve.js
+++ serve.js
@@ -3,5 +3,6 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname + '/'))
+app.use(express.static(process.cwd() + '/assets'))
 
 app.listen(process.env.PORT || 3000)
