d6ceace3aa689a6a0d2811af117c36d1d2bb54ca
mkstix6 <mkstix6@gmail.com>
2017-02-18 15:00:04 +0000

    adjustment

--- serve.js
+++ serve.js
@@ -1,6 +1,5 @@
 var connect = require('connect')
 var serveStatic = require('serve-static')
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html')
+connect().use(serveStatic(__dirname)).listen(8080, function () {
+  console.log('Server running on 8080...')
 })
