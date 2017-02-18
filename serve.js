f5814880a9b85e3c2cd617d43584fb40ff4b67c7
mkstix6 <mkstix6@gmail.com>
2017-02-18 10:57:24 +0000

    adjustment

--- serve.js
+++ serve.js
@@ -1,8 +1,11 @@
+// Load the http module to create an http server.
 var http = require('http')
-var fs = require('fs')
-var index = fs.readFileSync('index.html')
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end(index)
-}).listen(9615)
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.end('Hello World\n')
+})
+
+// Last, but not least, listen on port 8080
+server.listen(8080)
