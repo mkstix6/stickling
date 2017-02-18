f305ca12160e3a5cee9c205af5e7b08cf2ba01bf
mkstix6 <mkstix6@gmail.com>
2017-02-18 15:38:39 +0000

    webpack add

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var precss = require('precss')
+var autoprefixer = require('autoprefixer')
+var style = require('style-loader')
+var css = require('css-loader')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var path = '/'
+
+module.exports = {
+  entry: {
+    js: './assets/js/index.js',
+    css: './assets/scss/index.scss'
+  },
+  output: {
+    path: __dirname + 'dist',
+    filename: '[name].entry.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'sass']
+    }]
+  }
+}
