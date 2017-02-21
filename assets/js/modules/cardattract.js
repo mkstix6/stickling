3cc568efd1a140d73bd752873564c4c354ed0804
mkstix6 <mkstix6@gmail.com>
2017-02-21 23:33:14 +0000

    shine/shimmer added to person circle on transform

--- assets/js/modules/cardattract.js
+++ assets/js/modules/cardattract.js
@@ -2,21 +2,23 @@
 
 let mposit = [0, 0]
 
-document.onmousemove = handleMouseMove;
+document.onmousemove = handleMouseMove
 
-function handleMouseMove(event) {
-    mposit = [event.pageX - 500, event.pageY - 500]
+function handleMouseMove (event) {
+  mposit = [event.pageX - 500, event.pageY - 500]
 }
 
-function cardAttract() {
-    let card = document.getElementById('mecard')
+function cardAttract () {
+  let card = document.getElementById('mecard'),
+    shimmer = document.getElementById('shimmer')
 
-    function renderattract() {
-        card.style.transform = "rotateX(" + (mposit[1] * 0.01) + "deg) rotateY(" + (mposit[0] * -0.01) + "deg)"
+  function renderattract () {
+    card.style.transform = 'rotateX(' + (mposit[1] * 0.01) + 'deg) rotateY(' + (mposit[0] * -0.01) + 'deg)'
+    shimmer.style.transform = 'translateZ(0px) translateY(' + (mposit[1] * 0.1 - 100) + 'px) translateX(' + (mposit[0] * 0.1) + 'px)'
 
-        window.requestAnimationFrame(renderattract)
-    }
     window.requestAnimationFrame(renderattract)
+  }
+  window.requestAnimationFrame(renderattract)
 }
 
-module.exports = cardAttract;
+module.exports = cardAttract
