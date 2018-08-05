09a378cf936dec8821be16520fd9c20a19a646a1
mkstix6 <mkstix6@gmail.com>
2018-08-05 20:41:23 +0100

    Set up basic blog infrastructure

---
+++ src/components/TopNav.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/writing">writing</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+  name: 'TopNav',
+
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-import About from './views/About.vue';
+import Writing from './views/Writing.vue';
+import Article from './views/Article.vue';
 
 Vue.use(Router);
 
@@ -13,9 +14,17 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: About,
+      path: '/writing',
+      name: 'writing',
+      component: Writing,
+    },
+    {
+      path: '/writing/:id',
+      name: 'article',
+      props: true,
+      component: Article
     },
   ],
 });
+
+
