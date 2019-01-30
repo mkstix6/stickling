dabb54fd4bd4ecb1d5df2fdfac02a925eb627ba1
mkstix6 <mkstix6@gmail.com>
2019-01-30 22:58:01 +0000

    File format

--- src/store.ts
+++ src/store.ts
@@ -5,116 +5,113 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    skills: [{
-      name: 'HTML',
-      value: 4,
-    },
-    {
-      name: 'CSS',
-      value: 5,
-    },
-    {
-      name: 'Scss',
-      value: 4,
-    },
-    {
-      name: 'JS',
-      value: 3,
-    },
-    {
-      name: 'ES6',
-      value: 3,
-    },
-    {
-      name: 'Vue',
-      value: 4,
-    },
-    {
-      name: 'Nuxt',
-      value: 4
-    },
-    {
-      name: 'Bulma',
-      value: 4
-    },
-    {
-      name: 'jQuery',
-      value: 4,
-    },
-    {
-      name: 'GSAP',
-      value: 3,
-    },
-    {
-      name: 'TypeScript',
-      value: 1,
-    },
-    {
-      name: 'ScrollMagic',
-      value: 3,
-    },
-    {
-      name: 'npm',
-      value: 3,
-    },
-    {
-      name: 'yarn',
-      value: 3,
-    },
-    {
-      name: 'Git',
-      value: 4,
-    },
-    {
-      name: 'Gulp',
-      value: 3,
-    },
-    {
-      name: 'Webpack',
-      value: 3,
-    },
-    {
-      name: 'Rollup',
-      value: 3,
-    },
-    {
-      name: 'Docker',
-      value: 2,
-    },
-    {
-      name: 'React',
-      value: 1,
-    },
-    {
-      name: 'Bootstrap',
-      value: 4,
-    },
-    {
-      name: 'Zurb Foundation',
-      value: 3,
-    },
-    {
-      name: 'Photography',
-      value: 3,
-    },
-    {
-      name: 'PhotoShop',
-      value: 3,
-    },
-    {
-      name: 'Illustrator',
-      value: 3,
-    },
-    {
-      name: 'Sketch',
-      value: 3,
-    },
+    skills: [
+      {
+        name: 'HTML',
+        value: 4
+      },
+      {
+        name: 'CSS',
+        value: 5
+      },
+      {
+        name: 'Scss',
+        value: 4
+      },
+      {
+        name: 'JS',
+        value: 3
+      },
+      {
+        name: 'ES6',
+        value: 3
+      },
+      {
+        name: 'Vue',
+        value: 4
+      },
+      {
+        name: 'Nuxt',
+        value: 4
+      },
+      {
+        name: 'Bulma',
+        value: 4
+      },
+      {
+        name: 'jQuery',
+        value: 4
+      },
+      {
+        name: 'GSAP',
+        value: 3
+      },
+      {
+        name: 'TypeScript',
+        value: 1
+      },
+      {
+        name: 'ScrollMagic',
+        value: 3
+      },
+      {
+        name: 'npm',
+        value: 3
+      },
+      {
+        name: 'yarn',
+        value: 3
+      },
+      {
+        name: 'Git',
+        value: 4
+      },
+      {
+        name: 'Gulp',
+        value: 3
+      },
+      {
+        name: 'Webpack',
+        value: 3
+      },
+      {
+        name: 'Rollup',
+        value: 3
+      },
+      {
+        name: 'Docker',
+        value: 2
+      },
+      {
+        name: 'React',
+        value: 1
+      },
+      {
+        name: 'Bootstrap',
+        value: 4
+      },
+      {
+        name: 'Zurb Foundation',
+        value: 3
+      },
+      {
+        name: 'Photography',
+        value: 3
+      },
+      {
+        name: 'PhotoShop',
+        value: 3
+      },
+      {
+        name: 'Illustrator',
+        value: 3
+      },
+      {
+        name: 'Sketch',
+        value: 3
+      }
     ],
   },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  mutations: {},
+  actions: {}
 });
