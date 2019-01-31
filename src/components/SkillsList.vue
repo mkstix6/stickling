850b20a03b758cb8cd7bf88bd235be6385d4aea9
mkstix6 <mkstix6@gmail.com>
2019-01-31 00:47:09 +0000

    SkillsList file format and CSS class renaming

--- src/components/SkillsList.vue
+++ src/components/SkillsList.vue
@@ -1,7 +1,7 @@
 <template>
-  <ul class="person_attributes">
+  <ul class="skill_attributes">
     <li v-for="item in orderedItems" :key="item.name" class="starstat" :data-val="item.value">
-      <span class="text person_attributes" :title="starsTitle">{{item.name}}</span>
+      <span class="text skill_attributes">{{item.name}}</span>
     </li>
   </ul>
 </template>
@@ -38,31 +38,31 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-  @import '_variables.scss';
+@import '_variables.scss';
 
-  $sizeRate: 1.5;
-  @for $i from 1 through 5 {
-    [data-val="#{$i}"] {
-      font-size: $sizeRate * $i * 0.1rem + 1rem;
-    }
+$sizeRate: 1.5;
+@for $i from 1 through 5 {
+  [data-val='#{$i}'] {
+    font-size: $sizeRate * $i * 0.1rem + 1rem;
   }
+}
 
-  .person_attributes {
-    display: flex;
-    flex-wrap: wrap;
-    max-width: 50%;
-    width: 50%;
-    align-items: center;
-    justify-content: center;
-    white-space: nowrap;
+.skill_attributes {
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 50%;
+  width: 50%;
+  align-items: center;
+  justify-content: center;
+  white-space: nowrap;
 
-    @media (max-width: $media-lg) {
-      max-width:90%;
-      width: auto;
-    }
+  @media (max-width: $media-lg) {
+    max-width: 90%;
+    width: auto;
   }
+}
 
-  .starstat {
-    margin: 0.4vh 1vw;
-  }
+.starstat {
+  margin: 0.4vh 1vw;
+}
 </style>
