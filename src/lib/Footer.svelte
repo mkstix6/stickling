9be4a475823aca5d70d850f7eb7430b9ddcf6582
mkstix6 <mkstix6@gmail.com>
2021-07-19 23:07:39 +0100

    Header and Footer changes

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import Nav from '$lib/Nav.svelte';
+	import SocialNav from '$lib/SocialNav.svelte';
+	import LogoSticker from '$lib/LogoSticker.svelte';
+</script>
+
+<footer class="siteFooter">
+	<div>
+		<div class="bigM">
+			<div class="square-box">
+				<a href="/" title="home page" class="bigM__button logolink">
+					<svg height="224" viewBox="92 0 224 224" width="224" xmlns="http://www.w3.org/2000/svg">
+						<linearGradient id="stampGrad" x1="0%" y1="0%" x2="0%" y2="100%">
+							<stop offset="0" stop-color="#1d48b4" />
+							<stop offset="0.5" stop-color="#00b6ed" />
+							<stop offset="1" stop-color="#56ffd5" />
+						</linearGradient>
+						<g fill="url(#stampGrad)" fill-rule="evenodd">
+							<path
+								id="bigM"
+								d="m92.675712 212.753678 74.496027-210.09070058 35.503973 108.49999958 110-108.49999958-53.549459 216.99999958-31.818736-32.286241-41.456622 25.376942-31.523393-30.227054z"
+							/>
+						</g>
+					</svg>
+				</a>
+			</div>
+		</div>
+
+		<div>
+			<span>Get in touch…</span>
+			<SocialNav />
+		</div>
+
+		<Nav />
+
+		<div>
+			<div>
+				Built with <a href="https://kit.svelte.dev">SvelteKit</a>
+				<LogoSticker logokey="svelte" />
+			</div>
+		</div>
+	</div>
+</footer>
+
+<style lang="scss">
+	.siteFooter {
+		// display: flex;
+		// flex-direction: column;
+		// align-items: center;
+		// justify-content: space-between;
+		background: var(--card-backgroundcolor);
+
+		padding: 5rem 0.5rem 3rem;
+		margin-top: auto !important;
+
+		> div {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			margin-bottom: 1.5em;
+		}
+	}
+	.logolink {
+		cursor: pointer;
+
+		&:focus,
+		&:hover {
+			&,
+			& .bigM__button {
+				background: var(--pallette-active);
+			}
+		}
+	}
+	.bigM {
+		width: 3em;
+	}
+
+	.bigM svg {
+		display: block;
+		width: 80%;
+		height: 80%;
+	}
+
+	.bigM__button {
+		background: white;
+		border-radius: 100%;
+		border: 1px solid black;
+		aspect-ratio: 1 / 1;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,124 @@
+<script lang="ts">
+	import Nav from '$lib/Nav.svelte';
+	import SocialNav from '$lib/SocialNav.svelte';
+	// import ThemeSwitch from '$lib/ThemeSwitch.svelte';
+</script>
+
+<a class="skip-to-content-link" href="#main">Skip to main</a>
+
+<header class="siteHeader">
+	<div class="bigM">
+		<div class="square-box pos-rel">
+			<a href="/" title="home page" class="bigM__button logolink">
+				<svg viewBox="70 -20 260 260" height="200" width="200" xmlns="http://www.w3.org/2000/svg">
+					<linearGradient id="stampGrad" x1="0%" y1="0%" x2="0%" y2="100%">
+						<stop offset="0" stop-color="#1d48b4" />
+						<stop offset="0.5" stop-color="#00b6ed" />
+						<stop offset="1" stop-color="#56ffd5" />
+					</linearGradient>
+					<path
+						id="bigMStroke"
+						d="m92.675712 212.753678 74.496027-210.09070058 35.503973 108.49999958 110-108.49999958-53.549459 216.99999958-31.818736-32.286241-41.456622 25.376942-31.523393-30.227054z"
+						stroke="white"
+						stroke-width="35"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<g fill="url(#stampGrad)" fill-rule="evenodd">
+						<path
+							id="bigM"
+							d="m92.675712 212.753678 74.496027-210.09070058 35.503973 108.49999958 110-108.49999958-53.549459 216.99999958-31.818736-32.286241-41.456622 25.376942-31.523393-30.227054z"
+						/>
+					</g>
+				</svg>
+			</a>
+		</div>
+	</div>
+
+	<div class="navcontainer">
+		<Nav />
+		<SocialNav />
+		<!-- <ThemeSwitch /> -->
+	</div>
+</header>
+
+<style lang="scss">
+	.bigM {
+		width: 10vw;
+		position: absolute;
+		top: 1rem;
+		left: 1rem;
+		z-index: 10;
+		filter: drop-shadow(0 1px #0002);
+	}
+
+	.bigM svg {
+		display: block;
+		width: 80%;
+		height: 80%;
+	}
+
+	.bigM__button {
+		// background: white;
+		border-radius: 100%;
+		aspect-ratio: 1 / 1;
+		width: 100%;
+		height: 100%;
+		// position: absolute;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		filter: drop-shadow(0 1px #0002);
+		// background: var(--pallettemax);
+	}
+
+	.navcontainer {
+		display: flex;
+		align-items: stretch;
+		justify-content: space-between;
+		padding-left: 12vw;
+	}
+	.siteHeader {
+		background: var(--headerBGcolor);
+		padding: 0.5rem;
+		position: relative;
+
+		&::after {
+			content: '';
+			position: absolute;
+			top: 100%;
+			left: 0;
+			right: 0;
+			width: 0;
+			height: 0;
+			border: 0px solid transparent;
+			border-color: var(--headerBGcolor) transparent transparent transparent;
+			border-top-width: 10.5vw;
+			border-bottom-width: 0;
+			border-left-width: 0;
+			border-right-width: 100vw;
+			z-index: 0;
+		}
+	}
+
+	.logolink {
+		cursor: pointer;
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+
+		&:focus,
+		&:hover {
+			&,
+			& .bigM__button {
+				background: var(--pallette-active);
+			}
+		}
+	}
+
+	.socialnav a {
+		color: var(--pallette2);
+	}
+</style>
