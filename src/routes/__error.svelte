44126e046c7494cdd3ba78d1da131a06538c2599
mkstix6 <mkstix6@gmail.com>
2021-07-20 02:44:57 +0100

    Slightly improve error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				title: `${status}: ${error.message}`
+			}
+		};
+	}
+</script>
+
+<script>
+	export let title;
+</script>
+
+<div class="error-container">
+	<h1 style="max-width: min(35ex, 80vw);">{title}</h1>
+</div>
+
+<style>
+	.error-container {
+		margin: 10vw;
+	}
+</style>
