8526edc8cb00fc2778fa633efabcba94ee99489c
mkstix6 <mkstix6@gmail.com>
2021-07-31 02:03:27 +0100

    New confetti experiment particle styles

--- src/routes/posts/confetti.svelte
+++ src/routes/posts/confetti.svelte
@@ -4,8 +4,11 @@
 
 	let mouseX;
 	let mouseY;
-	let numParticles = 50;
+	let numParticles = 150;
 	let particles = [];
+
+	let particleStyles = ['confetti', 'snow', 'snowcannon'];
+	let particlesChoice = particleStyles[0];
 
 	onMount(() => {
 		const dustProto = {
@@ -16,40 +19,110 @@
 			velocity: 5,
 			// t: 100,
 			gravity: 1,
+			rotate: 0,
 			elem: undefined,
 			destroy() {
 				this.elem.remove();
 			},
 			reset() {
 				this.opacity = 1;
-				this.setPosition(mouseX, mouseY);
+				this.gravity = 1;
+				switch (particlesChoice) {
+					case 'confetti': {
+						this.rotate = Math.random() * 360;
+						this.setPosition(mouseX, mouseY);
+						break;
+					}
+					case 'snow': {
+						this.gravity = 0.4;
+						let a = Math.random() * Math.PI * 2;
+						let l = Math.random() * 200;
+						let dx = l * Math.sin(a);
+						let dy = (l / 2) * Math.cos(a) + 30;
+						let cx = !!mouseX ? mouseX + dx : 200 + dx;
+						let cy = !!mouseY ? mouseY + dy : 200 + dy;
+						this.setPosition(cx, cy);
+						break;
+					}
+					default: {
+						this.setPosition(mouseX, mouseY);
+						break;
+					}
+				}
 			},
-			tick() {
+			tick(options = {}) {
 				if (this.opacity < 0.02) {
 					this.reset();
 				}
+				let minV = 1;
+
 				// this.t--;
-				const minV = 1;
-				this.velocity = this.velocity < minV ? minV : this.velocity - 0.1;
-				this.angle = this.angle + (Math.random() * 2 - 1) * 0.1;
+				switch (particlesChoice) {
+					case 'snowcannon': {
+						minV = 1;
+						this.velocity =
+							this.opacity === 1
+								? (this.velocity = 12)
+								: this.velocity < minV
+								? minV
+								: this.velocity - Math.random() * minV * 0.3;
+						this.angle =
+							this.angle < Math.PI / 2 || this.angle > Math.PI
+								? (Math.PI * 3) / 4 + Math.random() * 0.2
+								: this.angle - 0.001;
+						this.opacity = ((this.opacity - 0.01) * 100) / 100;
+						break;
+					}
+					case 'snow': {
+						minV = 0.25;
+						this.velocity =
+							this.opacity === 1
+								? 1
+								: this.velocity < minV
+								? minV
+								: this.velocity - Math.random() * 0.01;
+						// this.angle =
+						// 	this.angle < -Math.PI / 2 || this.angle > Math.PI / 2
+						// 		? -Math.PI / 2 + Math.random() * Math.PI
+						// 		: this.angle - 0.001;
+						this.angle =
+							this.opacity === 1
+								? Math.random() * Math.PI * 1 - Math.PI * 0.5
+								: this.angle + Math.random() * 0.2 - 0.1;
+
+						this.opacity = this.opacity - Math.random() * 0.02;
+						break;
+					}
+					case 'confetti': {
+						this.angle = this.angle + (Math.random() * 2 - 1) * 0.1;
+						this.velocity = this.velocity < minV ? minV : this.velocity - 0.1;
+						this.opacity = Math.floor((this.opacity - 0.01) * 100) / 100;
+						break;
+					}
+					default: {
+						break;
+					}
+				}
+
+				// Calculate new position
 				this.x = this.x + this.velocity * Math.sin(this.angle);
 				this.y = this.y + this.velocity * Math.sin(this.angle + Math.PI / 2) + this.gravity;
-				this.opacity = Math.floor((this.opacity - 0.01) * 100) / 100;
+
 				this.updateCSS();
 				return true;
 			},
 			updateCSS() {
 				this.elem.style.transform = `translateX(${Math.floor(this.x)}px) translateY(${Math.floor(
 					this.y
-				)}px)`;
+				)}px) rotate(${this.rotate}deg)`;
 				this.opacity = this.elem.style.opacity = this.opacity;
 			},
 			setPosition(x, y) {
 				this.angle = Math.random() * 360;
 				this.velocity = Math.random() * 3 + 3;
 				this.tOG = this.t;
-				this.x = x;
-				this.y = y;
+				this.x = !!x ? x : 200;
+				this.y = !!y ? y : 200;
 			},
 			create() {
 				this.elem = document.createElement('div');
@@ -61,7 +134,7 @@
 			},
 			start(x = 200, y = 200) {
 				this.create();
-				this.setPosition(x, y);
+				this.reset();
 				this.tick();
 			}
 		};
@@ -97,7 +170,31 @@
 </script>
 
 <h1 class="angletext">Confetti effect experiment</h1>
-<div class="confettiSpace card" on:mousemove={handleMouseMove} />
+
+<form id="particlesOptions" action="">
+	<fieldset>
+		<legend>Choose your particles…</legend>
+
+		{#each particleStyles as style}
+			<div>
+				<label for="choice-{style}">{style}</label>
+				<input
+					bind:group={particlesChoice}
+					id="choice-{style}"
+					type="radio"
+					name="choice"
+					value={style}
+				/>
+			</div>
+		{/each}
+	</fieldset>
+</form>
+
+<div
+	class="confettiSpace card"
+	data-particleschoice={particlesChoice}
+	on:mousemove={handleMouseMove}
+/>
 
 <SocialLink href="https://codepen.io/mkstix6/pen/arZWbE" service="codepen"
 	>This experiment is also visible on CodePen.</SocialLink
