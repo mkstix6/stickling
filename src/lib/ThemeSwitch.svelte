ce08cfd97bf0af8d91ffdc2c9ab3563ac34c2ee2
mkstix6 <mkstix6@gmail.com>
2021-08-03 15:19:01 +0100

    Simplify code to avoid type warning

--- src/lib/ThemeSwitch.svelte
+++ src/lib/ThemeSwitch.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import ShapePolygon from '$lib/ShapePolygon.svelte';
 
 	const size = 50;
@@ -8,31 +8,35 @@
 		return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 	}
 
-	$: theme = () => {
-		if (themePreference === undefined) {
-			return isDarkModeEnabled() ? 'dark' : 'light';
-		} else {
-			return themePreference;
-		}
-	};
+	let theme;
+	let sunShape;
 
-	$: sunShape = () => {
-		return theme === 'dark'
-			? {
-					polyStyle: 'fill: white; stroke: #0001; stroke-width:1',
-					pointCount: 20,
-					chosenShape: 'star',
-					innerRadius: 0.7,
-					randomness: 0,
-			  }
-			: {
-					polyStyle: 'fill: white; stroke: #0001; stroke-width:1',
-					pointCount: 20,
-					chosenShape: 'star',
-					innerRadius: 0.7,
-					randomness: 0,
-			  };
-	};
+	$: {
+		if (themePreference === undefined) {
+			theme = isDarkModeEnabled() ? 'dark' : 'light';
+		} else {
+			theme = themePreference;
+		}
+	}
+
+	$: {
+		sunShape =
+			theme === 'dark'
+				? {
+						polyStyle: 'fill: white; stroke: #0001; stroke-width:1',
+						pointCount: 20,
+						chosenShape: 'star',
+						innerRadius: 0.7,
+						randomness: 0,
+				  }
+				: {
+						polyStyle: 'fill: white; stroke: #0001; stroke-width:1',
+						pointCount: 20,
+						chosenShape: 'star',
+						innerRadius: 0.7,
+						randomness: 0,
+				  };
+	}
 
 	const switchTheme = () => {
 		if (themePreference !== 'dark') {
