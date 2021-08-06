c4f77d59f328786f34a4020bf0a5dc78b060a055
mkstix6 <mkstix6@gmail.com>
2021-08-06 18:49:45 +0100

    Add integration tests: all pages load successfully

---
+++ cypress/integration/general_spec.js
@@ -0,0 +1,11 @@
+import { postItems } from '../../src/lib/sitedata.js';
+
+const pages = ['/', '/posts', '/about', '/contact', ...postItems.map((post) => post.link)];
+
+describe('All pages load', () => {
+	pages.forEach((page) => {
+		it(`Page loads: ${page}`, () => {
+			cy.visit(page);
+		});
+	});
+});
