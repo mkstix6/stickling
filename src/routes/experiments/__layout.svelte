312c10e743d19e33a20e13bb5f99f4609d663e6a
mkstix6 <mkstix6@gmail.com>
2021-08-08 00:27:49 +0100

    Create test page for LaptopLogos component

---
+++ src/routes/experiments/__layout.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Experiments | Stickling.co.uk</title>
+</svelte:head>
+
+<div class="page page--experiment">
+	<main id="main">
+		<slot />
+	</main>
+</div>
+
+<style lang="scss">
+</style>
---
+++ src/routes/experiments/laptop-logos.svelte
@@ -0,0 +1,32 @@
+<script>
+	import LaptopLogos from '$lib/LaptopLogos.svelte';
+</script>
+
+<svelte:head>
+	<title>GSAP flying laptop logos | Experiments | Stickling.co.uk</title>
+</svelte:head>
+
+<main id="main">
+	<figure>
+		<LaptopLogos />
+		<figcaption>Each logo should fly in from the front then subtly snap into place.</figcaption>
+	</figure>
+</main>
+
+<style>
+	#main {
+		padding: 3rem 0;
+		max-width: 70ex;
+		width: 70vw;
+		margin: 0 auto;
+	}
+
+	figcaption {
+		display: block;
+		width: 100%;
+		text-align: left;
+		padding: 0.5em 1em;
+		border-radius: 1em;
+		background: var(--card-backgroundcolor);
+	}
+</style>
