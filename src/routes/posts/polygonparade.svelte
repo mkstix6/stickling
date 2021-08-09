d408eb65551bffd8be2178e2c59e36e2080af2e8
mkstix6 <mkstix6@gmail.com>
2021-08-09 19:39:44 +0100

    Move page to posts

--- src/routes/posts/polygonparade.svelte
+++ src/routes/posts/polygonparade.svelte
@@ -4,21 +4,24 @@
 </script>
 
 <div class="grid">
-	<ShapePolygon pointCount={3} />
-	<ShapePolygon pointCount={4} />
-	<ShapePolygon pointCount={5} />
-	<ShapePolygon pointCount={6} />
-	<ShapePolygon pointCount={7} />
-	<SunFlare />
+	<div><ShapePolygon pointCount={3} /></div>
+	<div><ShapePolygon pointCount={4} /></div>
+	<div><ShapePolygon pointCount={5} /></div>
+	<div><ShapePolygon pointCount={6} /></div>
+	<div><ShapePolygon pointCount={7} /></div>
+	<div><ShapePolygon pointCount={8} innerRadius={0.7} chosenShape="star" /></div>
+	<div><SunFlare /></div>
 </div>
 
 <style>
 	.grid {
 		display: grid;
+		place-items: top center;
 		grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+		grid-template-rows: 200px;
 		/* This is better for small screens, once min() is better supported */
 		/* grid-template-columns: repeat(auto-fill, minmax(min(200px, 100%), 1fr)); */
 		gap: 1rem;
-		margin: 5vw;
+		margin: 5rem 5vw;
 	}
 </style>
