59c4bed2d38077215b105698c87ee8618fc889ea
mkstix6 <mkstix6@gmail.com>
2021-09-24 17:24:26 +0100

    Smaller video thumbnail for Slimes post

--- src/lib/sitedata.ts
+++ src/lib/sitedata.ts
@@ -760,7 +760,7 @@ export const postItems: PostItem[] = [
 		slug: 'slimes',
 		type: 'codeart',
 		tech: ['blender'],
-		videothumbnail: '/posts/SlimesRender-2021090102-720p.mp4',
+		videothumbnail: '/posts/slimes-small.mp4',
 		codethumbnail: `background: radial-gradient(center center, pink, pink 50%, blue 50%);`,
 	},
 	{
