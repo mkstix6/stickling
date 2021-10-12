f66cf6cf67163569eded4e203429d0640c0792ec
mkstix6 <mkstix6@gmail.com>
2021-10-12 18:32:19 +0100

    Art test page

--- src/routes/experiments/game.svelte
+++ src/routes/experiments/game.svelte
@@ -17,8 +17,9 @@
 		return [hue, 100, lightness];
 	}
 
-	let timeTravel = 190;
-	let daysRange = 28;
+	let daysRange = 401;
+	let timeJump = 0; //daysRange * 2;
+	let timeTravel = daysRange * timeJump;
 	let datesArray = Array.from(new Array(daysRange), (x, i) => {
 		var d = new Date();
 		d.setDate(d.getDate() + i + timeTravel);
@@ -26,35 +27,34 @@
 			d.toISOString().replaceAll('-', '').replaceAll('T', '').replaceAll(':', '').slice(0, 8)
 		);
 	});
+
+	let sizes = [9, 10, 11, 12, 13];
 </script>
 
 <div class="p5 m5" />
-<div class="m5">
-	<!-- <div>
-		{#each Array(360) as _, i}
-			<span
-				style="background: hsl({hueAdjust(i)[0]},{hueAdjust(i)[1]}%,{hueAdjust(
-					i
-				)[2]}%); border-radius: 2em;"
-				>{i}
-			</span>
-		{/each}
-	</div> -->
 
+<div class="m5">
 	<div class="grid">
-		<!-- {#each Array(12) as _, i} -->
 		{#each datesArray as date}
 			<div>
 				{date.toString().slice(6, 8)}/{date.toString().slice(4, 6)}/{date.toString().slice(0, 4)}
 				<div class="artcontainer">
-					<CanvasArt seed={date} />
+					<CanvasArt seed={date} rendersize={2 ** 9} diagnostics={false} />
 				</div>
 			</div>
 		{/each}
 	</div>
+
+	<!-- <div class="group grid">
+		{#each sizes as size}
+			<div class="artcontainer">
+				<CanvasArt seed={20260518} rendersize={2 ** size} diagnostics={true} />
+			</div>
+		{/each}
+	</div> -->
 </div>
 
-<style>
+<style lang="scss">
 	.grid {
 		display: flex;
 		flex-wrap: wrap;
@@ -63,17 +63,27 @@
 		align-content: flex-start;
 	}
 
+	.artcontainer {
+		--artsize: 256px;
+		width: var(--artsize);
+		height: auto;
+		// margin: 5em;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.group {
+		margin-bottom: 3em;
+	}
 	.grid .artcontainer {
-		--size: 512px;
+		--size: 256px;
 		/* --size: auto; */
 		flex: 0 0 var(--size);
 		width: var(--size);
 		height: var(--size);
 		height: auto;
 		box-sizing: content-box;
-		/* background: white; */
 		aspect-ratio: 1/1;
-		/* padding: calc(var(--size) / 16); */
 	}
 
 	.halfs {
