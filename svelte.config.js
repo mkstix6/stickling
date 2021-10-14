c29fea85d08bb72de85104d3cbc771d18d8d3442
mkstix6 <mkstix6@gmail.com>
2021-10-14 19:53:02 +0100

    Upgraded SvelteKit Netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,15 +8,15 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: netlify(),
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		vite: {
 			optimizeDeps: {
-				include: ['highlight.js/lib/core']
-			}
-		}
-	}
+				include: ['highlight.js/lib/core'],
+			},
+		},
+	},
 };
 
 export default config;
