7ba7a56abfd20dc9ba2f1d6f1c93d80b77592428
mkstix6 <mkstix6@gmail.com>
2022-01-21 17:52:30 +0000

    Improve site integration test coverage

--- cypress/integration/general_spec.js
+++ cypress/integration/general_spec.js
@@ -9,3 +9,34 @@ describe('All pages load', () => {
 		});
 	});
 });
+
+describe('Posts pages', () => {
+	it(`Newer Older navigation works`, () => {
+		cy.visit('/posts/confetti');
+		cy.get('footer .prevnext').contains(`Newer`);
+		cy.get('footer .prevnext').contains(`Older`);
+	});
+});
+
+describe('BubbleString art page', () => {
+	it(`Vanilla page loads today's art`, () => {
+		cy.visit('/posts/bubblestring-generative-art');
+		cy.contains(`Today's BubbleString`);
+	});
+	it(`URLquery chooses art`, () => {
+		cy.visit('/posts/bubblestring-generative-art?seed=1234');
+		cy.contains(`BubbleString: 1234`);
+	});
+});
+
+describe('SpiroFlare art page', () => {
+	it(`Vanilla page loads today's art`, () => {
+		cy.visit('/posts/spiroflare-generative-art');
+		cy.get('h2').contains(`TorusSpectre`);
+	});
+	it(`URLquery chooses art`, () => {
+		cy.visit('/posts/spiroflare-generative-art?preset=5&seed=1234');
+		cy.get('h2').contains(`SpiroCoil`);
+		cy.get('h2').contains(`#1234`);
+	});
+});
