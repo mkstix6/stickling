aab95441def05869c3952774e4a3438a971b575e
mkstix6 <mkstix6@gmail.com>
2022-05-02 17:27:58 +0100

    Add WebM video versions to Canvas Art post

--- src/routes/posts/canvas-art-2022.svelte
+++ src/routes/posts/canvas-art-2022.svelte
@@ -4,7 +4,8 @@
 	type ArtWork = {
 		title: string;
 		date: string;
-		localVideoSource: string;
+		localVideoSourceMP4: string;
+		localVideoSourceWEBM: string;
 		poster: string;
 		instagramURL: string;
 		codepenURL: string;
@@ -14,7 +15,8 @@
 		{
 			title: 'Calibrating Webb',
 			date: '15/02/2022',
-			localVideoSource: '/posts/code-art-2022/calibrating-webb.mp4',
+			localVideoSourceWEBM: '/posts/code-art-2022/calibrating-webb.webm',
+			localVideoSourceMP4: '/posts/code-art-2022/calibrating-webb.mp4',
 			poster: '/posts/code-art-2022/posters/calibrating-webb.jpeg',
 			instagramURL: 'https://www.instagram.com/p/CdA33uzD5XX/',
 			codepenURL: 'https://codepen.io/mkstix6/pen/abVyGow',
@@ -22,7 +24,8 @@
 		{
 			title: 'Dahlia Dance',
 			date: '13/02/2022',
-			localVideoSource: '/posts/code-art-2022/dahliadance01.mp4',
+			localVideoSourceWEBM: '/posts/code-art-2022/dahliadance01.webm',
+			localVideoSourceMP4: '/posts/code-art-2022/dahliadance01.mp4',
 			poster: '/posts/code-art-2022/posters/dahliadance01.jpeg',
 			instagramURL: 'https://www.instagram.com/p/CZ7qYaWrBz6/',
 			codepenURL: 'https://codepen.io/mkstix6/details/KKyvvyN',
@@ -30,7 +33,8 @@
 		{
 			title: 'Silver Ripple',
 			date: '12/02/2022',
-			localVideoSource: '/posts/code-art-2022/silverripple.mp4',
+			localVideoSourceWEBM: '/posts/code-art-2022/silverripple.webm',
+			localVideoSourceMP4: '/posts/code-art-2022/silverripple.mp4',
 			poster: '/posts/code-art-2022/posters/silverripple.jpeg',
 			instagramURL: 'https://www.instagram.com/p/CZ4_gXqD9R-/',
 			codepenURL: 'https://codepen.io/mkstix6/pen/dyZzbVY',
@@ -38,7 +42,8 @@
 		{
 			title: 'Hively',
 			date: '11/02/2022',
-			localVideoSource: '/posts/code-art-2022/hively.mp4',
+			localVideoSourceWEBM: '/posts/code-art-2022/hively.webm',
+			localVideoSourceMP4: '/posts/code-art-2022/hively.mp4',
 			poster: '/posts/code-art-2022/posters/hively.jpeg',
 			instagramURL: 'https://www.instagram.com/p/CZ2SUTFD-tq/',
 			codepenURL: 'https://codepen.io/mkstix6/pen/ExbXvgE',
@@ -46,7 +51,8 @@
 		{
 			title: 'Midnight Crystal Field',
 			date: '10/02/2022',
-			localVideoSource: '/posts/code-art-2022/midnight-crystal-field.mp4',
+			localVideoSourceWEBM: '/posts/code-art-2022/midnight-crystal-field.webm',
+			localVideoSourceMP4: '/posts/code-art-2022/midnight-crystal-field.mp4',
 			poster: '/posts/code-art-2022/posters/midnight-crystal-field.jpeg',
 			instagramURL: 'https://www.instagram.com/p/CZzA7pXjOc8/',
 			codepenURL: 'https://codepen.io/mkstix6/pen/dyZWPbr',
@@ -54,7 +60,8 @@
 		{
 			title: 'Party Refraction',
 			date: '07/02/2022',
-			localVideoSource: '/posts/code-art-2022/party-refraction.mp4',
+			localVideoSourceWEBM: '/posts/code-art-2022/party-refraction.webm',
+			localVideoSourceMP4: '/posts/code-art-2022/party-refraction.mp4',
 			poster: '/posts/code-art-2022/posters/party-refraction.jpeg',
 			instagramURL: 'https://www.instagram.com/p/CZsW8d-jtO9/',
 			codepenURL: 'https://codepen.io/mkstix6/pen/NWwbOGm',
@@ -62,7 +69,8 @@
 		{
 			title: 'Refraction Field',
 			date: '07/02/2022',
-			localVideoSource: '/posts/code-art-2022/magicrefractionfield.mp4',
+			localVideoSourceWEBM: '/posts/code-art-2022/magicrefractionfield.webm',
+			localVideoSourceMP4: '/posts/code-art-2022/magicrefractionfield.mp4',
 			poster: '/posts/code-art-2022/posters/magicrefractionfield.jpeg',
 			instagramURL: 'https://www.instagram.com/p/CZrk05xj5DF/',
 			codepenURL: 'https://codepen.io/mkstix6/pen/wvPoRjX',
@@ -70,7 +78,8 @@
 		{
 			title: 'Tree Growth',
 			date: '26/01/2022',
-			localVideoSource: '/posts/code-art-2022/treegrowth.mp4',
+			localVideoSourceWEBM: '/posts/code-art-2022/treegrowth.webm',
+			localVideoSourceMP4: '/posts/code-art-2022/treegrowth.mp4',
 			poster: '/posts/code-art-2022/posters/treegrowth.jpeg',
 			instagramURL: 'https://www.instagram.com/p/CZNVEL7lSaR/',
 			codepenURL: null,
@@ -78,15 +87,26 @@
 		{
 			title: 'Snowflake Drift',
 			date: '10/12/2021',
-			localVideoSource: '/posts/code-art-2022/snowflake-drift-20211210.mp4',
+			localVideoSourceWEBM: '/posts/code-art-2022/snowflake-drift-20211210.webm',
+			localVideoSourceMP4: '/posts/code-art-2022/snowflake-drift-20211210.mp4',
 			poster: '/posts/code-art-2022/posters/snowflake-drift-20211210.jpeg',
 			instagramURL: 'https://www.instagram.com/tv/CXUVR-KD1GW/',
 			codepenURL: null,
 		},
 		{
+			title: 'Grassy Green',
+			date: '23/11/2021',
+			localVideoSourceWEBM: '/posts/code-art-2022/grassy-green.webm',
+			localVideoSourceMP4: '/posts/code-art-2022/grassy-green.mp4',
+			poster: '/posts/code-art-2022/posters/grassy-green.jpeg',
+			instagramURL: 'https://www.instagram.com/tv/CWn9NtZDdkK/',
+			codepenURL: 'https://codepen.io/mkstix6/pen/ExvBYVK',
+		},
+		{
 			title: 'Chromie Wiggly',
 			date: '19/11/2021',
-			localVideoSource: '/posts/code-art-2022/chromiewigglyloop01.mp4',
+			localVideoSourceWEBM: '/posts/code-art-2022/chromiewigglyloop01.webm',
+			localVideoSourceMP4: '/posts/code-art-2022/chromiewigglyloop01.mp4',
 			poster: '/posts/code-art-2022/posters/chromiewigglyloop01.jpeg',
 			instagramURL: null,
 			codepenURL: null,
@@ -103,23 +123,17 @@
 </p>
 
 <ul>
-	{#each artworks as { title, date, localVideoSource, poster, instagramURL, codepenURL }}
+	{#each artworks as { title, date, localVideoSourceWEBM, localVideoSourceMP4, poster, instagramURL, codepenURL }}
 		<li>
 			<figure>
-				<video
-					preload="none"
-					{poster}
-					src={localVideoSource}
-					controls
-					loop
-					muted
-					playsinline
-					width="480"
-					height="480"
-				/>
+				<video preload="none" {poster} controls loop muted playsinline width="480" height="480">
+					<source src={localVideoSourceWEBM} type="video/webm" />
+					<source src={localVideoSourceMP4} type="video/mp4" />
+					{title}
+				</video>
 				<figcaption>
 					<div>
-						<span>{title} {date}</span>
+						<span>{title}</span>
 					</div>
 					<div>
 						{#if instagramURL}
