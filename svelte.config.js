65a89700ddd3a6b0cee1d444c60197487288ba2c
mkstix6 <mkstix6@gmail.com>
2023-01-28 19:49:55 +0000

    Copy over new SvelteKit config files

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,19 @@
 import adapter from '@sveltejs/adapter-netlify';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app
-			split: false,
-		}),
-		vite: {
-			optimizeDeps: {
-				include: ['highlight.js', 'highlight.js/lib/core'],
-			},
-		},
+		adapter: adapter(),
+		// vite: {
+		// 	optimizeDeps: {
+		// 		include: ['highlight.js', 'highlight.js/lib/core'],
+		// 	},
+		// },
 	},
 };
 
