15329cecb44010d56891e540c11df047e5939850
mkstix6 <mkstix6@gmail.com>
2023-01-30 13:15:13 +0000

    Upgrade Cypress test config

--- cypress/e2e/general_spec.cy.js
+++ cypress/e2e/general_spec.cy.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 import { postItems } from '../../src/lib/sitedata';
 
 const pages = ['/', '/posts', '/about', '/contact', ...postItems.map((post) => post.link)];
@@ -21,11 +23,11 @@ describe('Posts pages', () => {
 describe('BubbleString art page', () => {
 	it(`Vanilla page loads today's art`, () => {
 		cy.visit('/posts/bubblestring-generative-art');
-		cy.contains(`Today's BubbleString`);
+		cy.get('h2').contains(`Today's BubbleString`);
 	});
 	it(`URLquery chooses art`, () => {
 		cy.visit('/posts/bubblestring-generative-art?seed=1234');
-		cy.contains(`BubbleString: 1234`);
+		cy.get('h2').contains(`BubbleString: 1234`);
 	});
 });
 
@@ -36,7 +38,12 @@ describe('SpiroFlare art page', () => {
 	});
 	it(`URLquery chooses art`, () => {
 		cy.visit('/posts/spiroflare-generative-art?preset=5&seed=1234');
-		cy.get('h2').contains(`SpiroCoil`);
+		cy.get('h2').contains(`TorusSpectre`);
 		cy.get('h2').contains(`#1234`);
 	});
+});
+
+describe(`Error pages`, () => {
+	it.todo(`404 page has appropriate message`);
+	// e.g. 404 Sorry, we couldn't find this page
 });
