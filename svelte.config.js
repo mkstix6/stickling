0a3747c7bf3da8b406d4e1b934ccc3c24981b4a4
Mark Stickling <mark.stickling@itv.com>
2024-01-27 23:14:53 +0000

    Update svelte config from Netlify docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,8 +12,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+        adapter: adapter({
+          edge: true,
+          split: true
+        })
+      }
 };
 
 export default config;
