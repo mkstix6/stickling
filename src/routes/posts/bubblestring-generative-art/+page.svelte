4295bb63cf45ed43f6f36fe259c4448f0614e42c
Mark Stickling <mark.stickling@itv.com>
2024-12-23 18:45:53 +0000

    Simplify BubbleString UI

--- src/routes/posts/bubblestring-generative-art/+page.svelte
+++ src/routes/posts/bubblestring-generative-art/+page.svelte
@@ -1,78 +1,11 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import { goto } from '$app/navigation';
-
-	export let data;
-
-	let seedQueryParam = $page?.url?.searchParams?.get('seed');
-	let seed = parseInt(seedQueryParam);
-
 	const artName = 'BubbleString';
 	const today = new Date();
-	let renderedSeed: number;
-	let inputSeed: number;
-	let currentDate = today;
-	let seedMode = 'dateday';
-
-	if (seed) {
-		seedMode = 'basicnumber';
-		renderedSeed = seed;
-		inputSeed = seed;
-	} else {
-		renderedSeed = jsDateToSeed(currentDate);
-	}
-
+	let seed = $state(1);
+	const seedMode = 'basicnumber';
 	import LogoSticker from '$lib/LogoSticker.svelte';
 	import CanvasArt from '$lib/CanvasGenerativeArt01.svelte';
-
 	const renderSize = 2 ** 11;
-
-	let inputChosenDate = jsDateToInputString(currentDate);
-
-	if (typeof String.prototype.replaceAll == 'undefined') {
-		String.prototype.replaceAll = function (match, replace) {
-			return this.replace(new RegExp(match, 'g'), () => replace);
-		};
-	}
-
-	function jsDateToSeed(date: Date): number {
-		return Math.floor(
-			parseInt(
-				`${date.toISOString()}`
-					.replaceAll('-', '')
-					.replaceAll('T', '')
-					.replaceAll(':', '')
-					.slice(0, 8),
-				10,
-			),
-		);
-	}
-
-	function jsDateToInputString(jsDate: Date) {
-		return `${jsDate.toISOString()}`.replaceAll('T', '').replaceAll(':', '').slice(0, 10);
-	}
-
-	function inputDateToSeed(dateString: string): number {
-		if (dateString) {
-			return Math.floor(parseInt(`${dateString}`.replaceAll('-', '')));
-		}
-		throw new Error('Unhandled date value');
-	}
-
-	function moveDate(moveAmount: number) {
-		let d = new Date(currentDate);
-		currentDate = new Date(d.setDate(d.getDate() + moveAmount));
-		inputChosenDate = jsDateToInputString(currentDate);
-		renderedSeed = inputDateToSeed(inputChosenDate);
-		seed = inputDateToSeed(inputChosenDate);
-	}
-
-	function changeSeed(newSeed: string) {
-		inputSeed = parseInt(newSeed);
-		renderedSeed = inputSeed;
-		seed = inputSeed;
-		goto(`bubblestring-generative-art?seed=${seed}`, { noscroll: true });
-	}
 </script>
 
 <h1 class="angletext"><LogoSticker logokey="js" /> {artName} generative art</h1>
@@ -91,55 +24,22 @@
 	</p>
 
 	<h2 style="text-align: center;">
-		{#if seedMode === 'dateday'}
-			🗓
-			{#if renderedSeed === jsDateToSeed(today)}
-				Today's {artName}
-			{:else}
-				{artName} for {renderedSeed.toString().slice(6, 8)}/{renderedSeed
-					.toString()
-					.slice(4, 6)}/{renderedSeed.toString().slice(0, 4)}
-			{/if}
-		{:else}
-			{artName}: {seed}
-		{/if}
+		{artName} ({seed})
 	</h2>
 
 	<div class="art">
-		{#key renderedSeed}
-			<CanvasArt seed={renderedSeed} {renderSize} download={true} />
-			<small style="float: right;">seed: <strong>{renderedSeed}</strong></small>
+		{#key seed}
+			<CanvasArt {seed} {renderSize} download={true} />
+			<small style="float: right;">seed: <strong>{seed}</strong></small>
 		{/key}
 	</div>
-	{#if seedMode === 'dateday'}
-		<div class="datebuttons">
-			<button onclick={() => moveDate(-1)} onkeypress={() => moveDate(-1)}>←</button>
-			<div>
-				pick another day <input
-					type="date"
-					bind:value={inputChosenDate}
-					onchange={() => {
-						currentDate = new Date(inputChosenDate);
-						seed = inputDateToSeed(inputChosenDate);
-						renderedSeed = inputDateToSeed(inputChosenDate);
-					}}
-				/>
-			</div>
-			<button onclick={() => moveDate(1)}>→</button>
+	<div class="datebuttons">
+		<button onclick={() => (seed -= 1)}>←</button>
+		<div>
+			choose another seed <input type="number" bind:value={seed} onchange={() => seed} />
 		</div>
-	{:else}
-		<div class="datebuttons">
-			<button onclick={() => changeSeed(parseInt(seed) - 1)}>←</button>
-			<div>
-				choose another seed <input
-					type="number"
-					bind:value={inputSeed}
-					onchange={() => changeSeed(parseInt(inputSeed))}
-				/>
-			</div>
-			<button onclick={() => changeSeed(parseInt(seed) + 1)}>→</button>
-		</div>
-	{/if}
+		<button onclick={() => (seed += 1)}>→</button>
+	</div>
 
 	<p>
 		Much of the insight, theory and technique I used for creating this came from reading the
